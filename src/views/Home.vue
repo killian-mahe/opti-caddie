c827dcb13f8227d3b2ed1cada1e4512c4b6f24a1
TinaelSanvoisin <31204613+TinaelSanvoisin@users.noreply.github.com>
2020-10-06 16:06:48 +0200

    Simple button

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,17 @@
 <template>
   <div class="home">
+     <SimpleButton name="Editer" class="h-6"></SimpleButton>
   </div>
 </template>
 
 <script>
+import SimpleButton from "../components/SimpleButton.vue"
 
 export default {
+
   name: 'Home',
   components: {
+    SimpleButton
   }
 }
 </script>
