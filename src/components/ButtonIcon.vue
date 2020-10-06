6be7525c2ae0f5a36b6f172167f0bf95855608b4
Faustin Bacher <f7bacher@enib.fr>
2020-10-06 16:47:59 +0200

    Login

---
+++ src/components/ButtonIcon.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <div class="buttonicon cursor-pointer inline-block bg-leclercOrange rounded-lg h-40 text-2xl mt-12">
+        <span>{{name}}</span>
+        <div class="ml-32">
+            <i :data-feather="icon" class="h-16 w-16"></i>
+        </div>
+    </div>
+  </div>
+</template>
+
+<script>
+
+import feather from "feather-icons"
+export default {
+  name: 'ButtonIcon',
+  components: {
+  },
+  props: [
+      "name",
+      "icon",
+  ],
+  mounted(){
+      feather.replace()
+  }
+}
+</script>
--- src/views/Login.vue
+++ src/views/Login.vue
@@ -1,14 +1,21 @@
 <template>
-  <div class="login">
-     <button class='text-red-700'>Click here</button>
+  <div>
+    <div class="login text-6xl object-center flex justify-center h-full">
+      Opti-caddie
+    </div>
+    <div class="login flex justify-center items-center h-full">
+      <ButtonIcon icon="camera" name="Scannez votre carte de fidélité" class="mr-16 px-16"></ButtonIcon>
+      <ButtonIcon icon="user" name="Connectez-vous à votre compte" class="ml-16 px-16"></ButtonIcon>
+    </div>
   </div>
 </template>
 
 <script>
-
+import ButtonIcon from "../components/ButtonIcon.vue"
 export default {
   name: 'Login',
   components: {
+    ButtonIcon,
   }
 }
 </script>
