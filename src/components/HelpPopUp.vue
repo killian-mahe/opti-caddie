c75f15f8ff2c7a4e6d8284ce5a717d0c778f37af
Tako <pierrelouis.hernio@gmail.com>
2020-10-13 14:41:19 +0200

    Added Navbar display exception + close popup fix

--- src/components/HelpPopUp.vue
+++ src/components/HelpPopUp.vue
@@ -1,19 +1,33 @@
 <template>
-  <div id="popup-wrapper" @click="close()">
-    <div id="popup">C'est très l'aide</div>
+  <div id="wrapper">
+    <div id="popup">
+      Démerde toi, on est pas tes parents
+      <div id="help-btn" class="cursor-pointer">
+        <i data-feather="bell" class="w-12 h-12"></i>
+      </div>
+    </div>
+
+    <div id="backdrop" @click="close()" class="cursor-pointer"></div>
   </div>
 </template>
 
 <style>
-#popup-wrapper {
+#backdrop {
+  @apply bg-black;
+  @apply bg-opacity-75;
+  @apply w-full;
+  @apply h-full;
+  @apply absolute;
+}
+
+#wrapper {
   width: inherit;
   height: inherit;
-  @apply fixed;
+  @apply absolute;
   @apply z-10;
-  display: flex;
+  @apply flex;
   justify-content: center;
   align-items: center;
-  background-color: rgba(0, 0, 0, 0.5);
 }
 
 #popup {
@@ -24,10 +38,28 @@
   @apply bg-white;
   @apply rounded-lg;
   @apply p-6;
+  @apply text-justify;
+  @apply relative;
+}
+
+#help-btn {
+  @apply w-16;
+  @apply h-16;
+  @apply bg-red-700;
+  @apply text-white;
+  @apply rounded-full;
+  @apply flex;
+  @apply absolute;
+  @apply inset-x-0;
+  @apply bottom-0;
+  justify-content: center;
+  align-items: center;
 }
 </style>
 
 <script>
+import feather from "feather-icons";
+
 export default {
   name: "HelpPopUp",
 
@@ -35,6 +67,10 @@ export default {
     close: function () {
       this.$emit("exit");
     },
+  },
+
+  mounted() {
+    feather.replace();
   },
 };
 </script>
