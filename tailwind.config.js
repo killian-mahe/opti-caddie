d3df4efa8cc9bc77a1ae86fd126513d6849e8bbb
Killian <killianmahe.pro@gmail.com>
2020-10-13 11:24:03 +0200

    💄 Adding Quicksand font

--- tailwind.config.js
+++ tailwind.config.js
@@ -269,6 +269,7 @@ module.exports = {
       ],
       serif: ['Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif'],
       mono: ['Menlo', 'Monaco', 'Consolas', '"Liberation Mono"', '"Courier New"', 'monospace'],
+      quicksand: ['Quicksand', 'sans-serif'],
     },
     fontSize: {
       xs: '0.75rem',
