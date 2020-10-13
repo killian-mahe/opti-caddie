5432b6a4a82b4077011c4a4c420b4d10ca9bc3af
TinaelSanvoisin <31204613+TinaelSanvoisin@users.noreply.github.com>
2020-10-13 16:22:36 +0200

    Ajout du carousel

---
+++ src/components/Carousel.vue
@@ -0,0 +1,55 @@
+<template>
+  <div class="carousel w-full">
+      <slot></slot>
+
+      <button @click="$emit('next')" class="next deco">Next</button>
+      <button @click="$emit('prev')" class="prev deco">Prev</button>
+  </div>
+</template>
+
+<script>
+
+
+  export default {
+    name: 'Carousel',
+    data() {
+      return {
+          
+        
+        }
+  },
+
+ 
+}
+</script>
+
+<style >
+.carousel{
+    position: relative;
+    height: 275px;
+    overflow: hidden;
+}
+
+.deco{
+    position: absolute;
+    height: 40px;
+    width: 50px;
+    top: calc(50% - 20px);
+    background-color: rgba(0,0,0,0.8);
+    border: none;
+    color: #FFF;
+}
+
+button:focus, button:hover{
+    outline: none;
+    cursor: pointer;
+}
+
+.next{
+    right:0;
+}
+
+.prev{
+    left: 0;
+}
+</style>
---
+++ src/components/CarouselSlide.vue
@@ -0,0 +1,20 @@
+<template>
+      <div v-show="visibleSlide === index" class="carousel-slide">
+          <slot></slot>
+      </div>
+
+    
+</template>
+
+
+<script>
+export default {
+    name: "CarouselSlide",
+    props: ['index', 'visibleSlide'],
+    data() {
+        return {
+
+        }
+    }
+}
+</script>
