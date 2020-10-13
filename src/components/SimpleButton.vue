8483a2148a8be0c4ac72c03b14901068cef024ad
Faustin Bacher <f7bacher@enib.fr>
2020-10-13 16:11:29 +0200

    Connexion

--- src/components/SimpleButton.vue
+++ src/components/SimpleButton.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="SimpleButton bg-leclercOrange hover:bg-leclercOrangeClair text-black font-semibold py-2 px-4 border-b-8 border-leclercOrangeFonce hover:border-leclercOrange rounded-lg text-center cursor-pointer">{{ name }}
+  <div @click="$emit('click')" class="SimpleButton bg-leclercOrange hover:bg-leclercOrangeClair text-black font-semibold py-2 px-4 border-b-8 border-leclercOrangeFonce hover:border-leclercOrange rounded-lg text-center cursor-pointer">{{ name }}
   </div>
 </template>
 
