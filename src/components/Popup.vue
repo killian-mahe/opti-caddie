cb153940b3c380cc8b99464206c53275d3867622
Faustin Bacher <f7bacher@enib.fr>
2020-10-20 11:23:25 +0200

    Popup & Fin ...

---
+++ src/components/Popup.vue
@@ -0,0 +1,67 @@
+<template>
+    <div class="Popup-backdrop ">
+      <div class="Popup pl-6 pr-6 pt-2 pb-2 text-4xl rounded-lg font-quicksand grid grid-rows-2" role="dialog">
+        <div class="self-center pl-5">
+              {{title}}
+        </div>
+        <div class="grid grid-cols-3">
+            <button type="button" class="btn-close px-0 bg-leclercOrange rounded-lg text-2xl mt-4 pr-2 pl-2 font-quicksand col-start-2" @click="close" aria-label="Close Popup">
+              Fermer
+            </button>
+        </div>
+          
+      </div>
+      
+    </div>
+</template>
+
+
+<script>
+  export default {
+    name: 'Popup',
+    methods: {
+      close() {
+        this.$emit('close');
+      },
+    },
+    components: {
+    },
+    props: [
+        "title",
+    ]
+    
+  };
+</script>
+
+<style>
+
+  .Popup-backdrop {
+    position: fixed;
+    top: 0;
+    bottom: 0;
+    left: 0;
+    right: 0;
+    background-color: rgba(0, 0, 0, 0.3);
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .Popup {
+    background: #FFFFFF;
+    box-shadow: 2px 2px 20px 1px;
+    overflow-x: auto;
+    display: flex;
+    flex-direction: column;
+  }
+
+  .Popup-close {
+    border: none;
+    font-size: 20px;
+    padding: 20px;
+    cursor: pointer;
+    font-weight: bold;
+    color: #4AAE9B;
+    background: transparent;
+  }
+</style>
--- src/views/Identifiants.vue
+++ src/views/Identifiants.vue
@@ -1,5 +1,5 @@
 <template>
-  <div>
+  <div class="font-quicksand">
     <img
       src="../assets/logo_leclerc.svg"
       class="object-scale-down inline-flex h-24 w-auto mr-auto ml-2 pt-2"
@@ -11,7 +11,7 @@
         style
       ></SimpleButton>
     </router-link>
-    <form class="bg-white rounded px-8 pt-6 pb-8 mb-4 mt-16 ml-20">
+    <form class="bg-white rounded px-8 pt-6 pb-8 mb-4 mt-16 ml-20 ">
       <div class="mb-4">
         <label
           class="block text-gray-700 text-3xl font-bold mb-2"
@@ -26,6 +26,7 @@
           placeholder="Nom d'utilisateur"
           v-model="username"
         />
+        <div v-show="noUsername" class="text-red-700">Veuillez entrer nom d'utilisateur !</div>
       </div>
       <div class="mb-6">
         <label
@@ -35,26 +36,29 @@
           Mot de passe
         </label>
         <input
-          class="text-3xl shadow appearance-none border w-6/12 py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+          class="text-3xl shadow appearance-none border w-6/12 py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
           id="password"
           type="password"
           placeholder="*******"
           v-model ="mdp"
         />
+        <div v-show="noMdp" class="text-red-700">Veuillez entrer un mot de passe !</div>
       </div>
       <div class="flex items-center justify-between">
         <SimpleButton
           name="Se connecter"
           class="w-48 inline-block text-xl"
           style
-          @click="connect"
+          @click="connect(); missingLogin()"
         ></SimpleButton>
         <SimpleButton
           name="Mot de passe oublié ?"
           class="w-48 inline-block text-xl mr-48"
           style
-           @click="mdpForgot"
+           @click="showPopup"
         ></SimpleButton>
+        <Popup title="Pas de chance" v-show="isPopupVisible" @close="closePopup"></Popup>
+        <Popup title="Les identifiants ne correspondent pas à un utilisateur enregistré" v-show="popupNoUser" @close="closePopup"></Popup>
       </div>
     </form>
   </div>
@@ -63,13 +67,16 @@
 <script>
 import { mapActions, mapState } from 'vuex';
 import SimpleButton from "../components/SimpleButton.vue";
+import Popup from '../components/Popup.vue';
+
 export default {
   name: "Identifiants",
   components: {
     SimpleButton,
+    Popup,
   },
   data(){
-      return {mdp:"", username:""}
+      return {mdp:"", username:"", isPopupVisible:false, noUsername:false, noMdp:false, popupNoUser:false}
   },
   computed:{
     ...mapState(["users", "session"])
@@ -82,9 +89,33 @@ export default {
         })
       if(user){
         this.updateLoggedUser(user.id)
+        this.popupNoUser=false;
+      }else{
+        this.popupNoUser=true;
       }
       console.log(this.session.user)
     },
+
+    missingLogin:function(){
+      if(this.username==''){
+        this.noUsername = true;
+      }else{
+        this.noUsername=false;
+      }
+      if(this.mdp==''){
+        this.noMdp = true;
+      }else{
+        this.noMdp=false;
+      }
+    },
+
+    showPopup() {
+      this.isPopupVisible = true;
+    },
+    closePopup() {
+      this.isPopupVisible = false;
+      this.popupNoUser =false;
+    }
   }
 };
 </script>
